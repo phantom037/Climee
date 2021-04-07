a3e9a9fbd95302388bf8ac554f518692c0516a32
Leo Tran <leotran@Leos-MacBook-Air.local>
2021-04-07 22:20:56 +0700

    First Commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,40 @@
+import 'package:clima/services/networking.dart';
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    WeatherModel weatherModel = new WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    })); //Navigator
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.greenAccent,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetWorkHelper {
+  NetWorkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      return decodedData;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
